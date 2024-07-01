05e8f288291eeb087f40c99243874ba9146bf5f1
SergeyGorinenko <sergey.gorinenko@gmail.com>
2024-06-30 18:01:27 -0700

    Added Country entity

---
+++ Modules/WorldleCore/Sources/WorldleCore/Classes/Models/CountryEntity.swift
@@ -0,0 +1,22 @@
+//
+//  CountryEntity.swift
+//
+//
+//  Created by Serhii Horinenko on 30.06.2024.
+//
+
+import CoreLocation
+import Foundation
+
+public struct CountryEntity: Identifiable {
+    public let id: String
+    public let name: String
+    public let location: CLLocation
+    
+    public init(id: String, name: String, location: CLLocation) {
+        self.id = id
+        self.name = name
+        self.location = location
+    }
+    
+}
